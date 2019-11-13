15dc679b4fa0140aac39093058aa5e65fb93e002
Terrence Eve <tijayeve@gmail.com>
2019-11-13 00:54:26 -0600

    wrote ES6 arrow functions

--- arrowFunctions/ex1.js
+++ arrowFunctions/ex1.js
@@ -7,13 +7,26 @@ let introduction = function(name, otherName) {
     return introduction;
 }
 
+const introduction = (name, otherName) => {
+    return name + ", let me introduce you to " + otherName
+}
+
 // This one only has one parameter! 
 let boringFunction = function (topic) {
     let complain = topic + " is so boooooring!!!";
     return complain;
 }
 
+const boringFunction = (topic) => {
+    let complain = topic + " is so boooooring!!!"
+    return complain;
+}
+
 // This one has a single return statement!
 let shout = function() {
+    return "AAAHHHH!!"
+}
+
+const shout = () => {
     return "AAAHHHH!!"
 }
